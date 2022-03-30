e005b3f5427aba37f34e609e02b0bb4c69dd5c4f
k-szymaniak <k-szymaniak@tlen.pl>
2022-03-30 21:49:55 +0200

    30.03.2022

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,37 @@
 import express from 'express'
 import {Request, Response} from 'express'
-import { text } from 'stream/consumers'
+
+interface date{
+  Id: number;
+  name: string;
+}
 
 const app = express()
-
 const date = new Date()
-let text2 : string
+const jsonData = JSON.stringify(date)
+const note = JSON.parse(jsonData)
+
+
+
 app.use(express.json())
 
-app.get('/note/:text2', function (req: Request, res: Response) {
-   // res.sendStatus(200).send(app)
-   res.send(text2)
-   console.log(text2)
+app.get('/note/:id', function (req: Request, res: Response) {
+  console.log(req.body) // e.x. req.body.title 
+  res.send(note)
+  res.sendStatus(200)
 })
 app.post('/note', function (req: Request, res: Response) {
-  console.log(req.body) // e.x. req.body.title 
-  console.log(res.header)
-  res.sendStatus(201).send(date.toISOString())
-  
+  console.log(req.body) // e.x. req.body.title śś
+  res.send(date)
+  res.sendStatus(201)
 })
 app.put('/note/:id', function (req: Request, res: Response) {
-    console.log(req.body) // e.x. req.body.title 
-    res.sendStatus(204).send(date.toISOString())
+  console.log(req.body) // e.x. req.body.title 
+  res.sendStatus(204).send("")
 })
 app.delete('/note/:id', function (req: Request, res: Response) {
-    console.log(req.body) // e.x. req.body.title 
-    res.sendStatus(204).send(date.toISOString())
+  console.log(req.body) // e.x. req.body.title 
+  res.sendStatus(204).send("")
 })
 
 app.listen(3000)
